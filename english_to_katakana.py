297c994cd4e3fd87cd4837a1ff769bccece42e08
Iori ONDA <iorionda@gmail.com>
2021-06-17 10:28:49 +0900

    :tada: 英語からカタカナの読みを取得する

---
+++ english_to_katakana.py
@@ -0,0 +1,20 @@
+# 英語→カタカナ変換機(https://www.sljfaq.org/cgi/e2k_ja.cgi)からスクレイピング
+import urllib
+from bs4 import BeautifulSoup
+
+def english_to_katakana(word):
+    url = 'https://www.sljfaq.org/cgi/e2k_ja.cgi'
+    url_q = url + '?word=' + word
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0'}
+
+    request = urllib.request.Request(url_q, headers=headers)
+    html = urllib.request.urlopen(request)
+    soup = BeautifulSoup(html, 'html.parser')
+    katakana_string = soup.find_all(class_='katakana-string')[0].string.replace('\n', '')
+
+    return katakana_string
+
+word = 'English'
+
+katakana_string = english_to_katakana(word)
+print(katakana_string)
